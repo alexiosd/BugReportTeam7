00b14e3cf9c9ce682f7163cfbd784075192ed1e0
Alexis Daloglou <enoa@hotmail.com>
2018-05-11 01:31:46 +0300

    Form validations

---
+++ src/app/layout/report-bug/bug.model.ts
@@ -0,0 +1,9 @@
+export class Bug {
+  constructor() {}
+
+  public title: string;
+  public description: string;
+  public priority: number;
+  public reporter: string;
+  public status: string;
+}
--- src/app/layout/report-bug/report-bug.component.ts
+++ src/app/layout/report-bug/report-bug.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Bug } from './bug.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'bgt7-report-bug',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportBugComponent implements OnInit {
 
+  model: Bug;
+  priorityValid: boolean;
+  reporterValid: boolean;
+  statusValid: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.model = new Bug();
+    this.model.priority = null;
+    this.model.reporter = null;
+    this.model.status = null;
+    debugger;
+  }
+
+  priorityIsValid(event) {
+    this.priorityValid = !(this.model.priority === null);
+  }
+
+  reporterIsValid(event) {
+    this.reporterValid = !(this.model.reporter === null);
+  }
+
+  statusIsValid(event) {
+    this.statusValid = !((this.model.status === null) && (this.model.reporter === 'QA'));
+  }
+
+  formSubmit(form: NgForm) {
+    this.priorityIsValid(this.model.priority);
+    this.reporterIsValid(this.model.reporter);
+    this.statusIsValid(this.model.status);
+  debugger;
+  if (!form.valid || !this.priorityValid || !this.reporterValid || !this.statusValid) {
+      console.log('error');
+      return;
+    }
+
+    console.log(form.value);
   }
 
 }
